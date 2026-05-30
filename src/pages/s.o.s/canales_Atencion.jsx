92d603803b6d2d5a327cde81807e762033b689b0
andres <af1088028@gmail.com>
2026-05-30 06:59:52 -0500

    cambio de nuemro

--- src/pages/s.o.s/canales_Atencion.jsx
+++ src/pages/s.o.s/canales_Atencion.jsx
@@ -103,7 +103,7 @@ function ConfirmModal({ isOpen, onConfirm, onCancel, title, description, confirm
 
 export default function EmergencyMode({
   userName = "Usuario",
-  trustedContact = { name: "Contacto de Confianza", phone: "tel:+1234567890" },
+  trustedContact = { name: "Contacto de Confianza", phone: "tel:3145190709" },
   onCallEmergency,
   onCallContact,
 }) {
